54ba74bb90530ca9ac5c1c15f5fbd9e78f6b5602
lccxxo <kafka.embracetheday@gmail.com>
2026-01-05 23:55:55 +0800

    fix：minigrep 使用match捕获args

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -12,15 +12,15 @@ impl Config{
     pub fn build(mut args: impl Iterator<Item = String>)-> Result<Config, &'static str>{
         args.next();
 
-        let query = args.next(){
+        let query = match args.next() {
             Some(arg) => arg,
             None => return Err("Didn't get a query string"),
-        }
+        };
 
-        let file_path = args.next(){
+        let file_path = match args.next() {
             Some(arg) => arg,
-            None => retrun Err("Didn't get a file path"),
-        }
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
