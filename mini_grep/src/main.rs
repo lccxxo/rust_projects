55d78afef1a53d8df1ccc535a86174afac17b4f9
majunhong <kafka.embracetheday@gmail.com>
2026-01-20 09:47:48 +0800

    fix: mini_grep

--- mini_grep/src/main.rs
+++ mini_grep/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use minigrep::Config;
+use mini_grep::Config;
 
 fn main(){
     // let args: Vec<String> = env::args().collect();
@@ -10,7 +10,7 @@ fn main(){
         process::exit(1);
     });
 
-    if let Err(e) = minigrep::run(config){
+    if let Err(e) = mini_grep::run(config){
         println!("Application error: {e}");
         process::exit(1);
     }
